340f897e13a541a70842c3e32cb146a2937ca78e
Existent_Chicken <isaiahpaultrue@gmail.com>
2025-12-18 11:44:07 -0800

    Update pxt.json, dictionaries.ts

--- dictionaries.ts
+++ dictionaries.ts
@@ -16,4 +16,16 @@ namespace dictionaries {
     export function get(name: string, valuename: string){
         return dictionaryDictionary[name][valuename]
     }
+    //% block="Create new dictionary inside of dictionary $name with name $valuename"
+    export function createNested(name: string, valuename: string) {
+        dictionaries.setoradd(valuename, {}, name)
+    }
+    //% block="Set/add value $valuename $value in nested dictionary $name that is in dictionary $name1"
+    export function setoraddnested(valuename: string, value: any, name: string, name1: string) {
+        dictionaryDictionary[name1][name][valuename] = value
+    }
+    //% block="Get value at nested dictionary $name that is is dictionary $name1 at value $valuename"
+    export function getNested(name:string,name1:string,valuename:string){
+        return dictionaryDictionary[name1][name][valuename]
+    }
 }
